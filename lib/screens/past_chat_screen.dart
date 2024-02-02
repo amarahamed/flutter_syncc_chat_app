8970df597cd174599536ab2ce832b6dc68201d23
Amar Ahamed <amarahamed0615@gmail.com>
2024-02-02 17:32:45 -0500

    figuring out how to update previous chats screen to update

--- lib/screens/past_chat_screen.dart
+++ lib/screens/past_chat_screen.dart
@@ -1,6 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:syncc_chat_app/screens/helper.dart';
+import 'package:syncc_chat_app/models/receiver.dart';
+import 'package:syncc_chat_app/screens/chat_screen.dart';
+import 'package:async/async.dart';
 import 'package:syncc_chat_app/services/authentication.dart';
+import 'package:syncc_chat_app/services/helper.dart';
 
 class PastChatsScreen extends StatefulWidget {
   const PastChatsScreen({super.key});
@@ -10,6 +15,89 @@ class PastChatsScreen extends StatefulWidget {
 }
 
 class _PastChatsScreenState extends State<PastChatsScreen> {
+  List<dynamic> allChatData = [];
+
+  // Future<List> checkPastChats() async {
+  //   String currentUserId = FirebaseAuth.instance.currentUser!.uid;
+  //   List<Stream<QuerySnapshot<Map<String, dynamic>>>> snapshots = [];
+  //
+  //   // get the past chat list from the user collection
+  //   var previousChatsPeople = await FirebaseFirestore.instance
+  //       .collection('users')
+  //       .doc(currentUserId)
+  //       .collection('chat')
+  //       .get();
+  //
+  //   final List previousChatsData =
+  //       previousChatsPeople.docs.map((doc) => doc).toList();
+  //
+  //   for (var pastChatUserId in previousChatsData) {
+  //     var listMessages = await FirebaseFirestore.instance
+  //         .collection('chats')
+  //         .doc('$currentUserId-${pastChatUserId.id}')
+  //         .collection('messages')
+  //         .get();
+  //
+  //     Stream<QuerySnapshot<Map<String, dynamic>>> x = FirebaseFirestore.instance
+  //         .collection('chats')
+  //         .doc('$currentUserId-${pastChatUserId.id}')
+  //         .collection('messages')
+  //         .snapshots();
+  //
+  //     snapshots.add(x);
+  //
+  //     // for (var element in listMessages.docs) {
+  //     //   print("-----> ${element.data()}");
+  //     // }
+  //
+  //     // for (var element in listMessages.docs) {
+  //     //   setState(() {
+  //     //     allChatData.add(element.data());
+  //     //   });
+  //     // }
+  //   }
+  //
+  //   return snapshots;
+  // }
+//Future<List<QuerySnapshot<Object>>>
+
+  void getSnapshots() async {
+    List<Stream<QuerySnapshot<Map<String, dynamic>>>> streams = [];
+
+    String currentUserId = FirebaseAuth.instance.currentUser!.uid;
+    var previousChatsPeople = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(currentUserId)
+        .collection('chat')
+        .get();
+    print("###");
+    final List previousChatsData =
+        previousChatsPeople.docs.map((doc) => doc).toList();
+    // loop through previousChatData and the element.id will give the id
+
+    for (var element in previousChatsData) {
+      // print(element.id);
+      var chatDocumentId = await Helper()
+          .checkChatDocument(senderId: currentUserId, receiverId: element.id);
+      print(chatDocumentId);
+
+      streams.add(FirebaseFirestore.instance
+          .collection('chats')
+          .doc(chatDocumentId)
+          .collection('messages')
+          .snapshots());
+    }
+  }
+
+  late Future<List> futureData;
+
+  @override
+  void initState() {
+    getSnapshots();
+    super.initState();
+    // futureData = checkPastChats();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,9 +109,10 @@ class _PastChatsScreenState extends State<PastChatsScreen> {
           color: Theme.of(context).colorScheme.secondary,
         ),
         onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return const PeopleLookUpScreen();
-          }));
+          getSnapshots();
+          // Navigator.push(context, MaterialPageRoute(builder: (context) {
+          //   return const PeopleLookUpScreen();
+          // }));
         },
       ),
       appBar: AppBar(
@@ -31,13 +120,50 @@ class _PastChatsScreenState extends State<PastChatsScreen> {
         centerTitle: true,
         actions: [
           IconButton(
-              onPressed: () async {
-                await Authentication().signOutUser();
-              },
-              icon: const Icon(Icons.exit_to_app_rounded)),
+            onPressed: () async {
+              await Authentication().signOutUser();
+            },
+            icon: const Icon(Icons.exit_to_app_rounded),
+          ),
         ],
       ),
-      body: const Center(child: Text('No Chats')),
+      body: allChatData.isEmpty
+          ? const Center(
+              child: Text('No Chats'),
+            )
+          : StreamBuilder(
+              stream: FirebaseFirestore.instance.collection('chats').doc(),
+              builder: (context, snapshot) {
+                return ListView.builder(
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: ListTile(
+                        title: Text(allChatData[index]['receiverUsername']),
+                        leading: CircleAvatar(
+                          foregroundImage:
+                              NetworkImage(allChatData[index]['receiverPfp']),
+                          radius: 50,
+                        ),
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            print("----------->");
+                            print(allChatData[index]);
+                            return ChatScreen(
+                                receiverData: ReceiverData(
+                                    uid: allChatData[index]['receiver'],
+                                    pfpUrl: allChatData[index]['receiverPfp'],
+                                    username: allChatData[index]
+                                        ['receiverUsername']));
+                          }));
+                        },
+                      ),
+                    );
+                  },
+                  itemCount: allChatData.length,
+                );
+              }),
     );
   }
 }
